0f2e3541bb9bde29596daed7a649d19cc1923016
Ivan Morozov <38524976+ivanmorozfd@users.noreply.github.com>
2020-03-11 11:29:41 +0300

    Naming fixes,its heap,lol

--- Main/Main/BinaryHeapExceptions.h
+++ Main/Main/BinaryHeapExceptions.h
@@ -4,18 +4,18 @@
 using std::string;
 using std::exception;
 
-class BinarySearchTreeException :exception
+class BinaryHeapException :exception
 {
 private:
 	std::string whatStr;
 public:
 	const char* what() const noexcept override;
 public:
-	BinarySearchTreeException(std::string&& whatStr) noexcept : whatStr(std::move(whatStr)) { }
-	BinarySearchTreeException(const std::string& whatStr) noexcept : whatStr(whatStr) { }
-	~BinarySearchTreeException() noexcept = default;
+	BinaryHeap(std::string&& whatStr) noexcept : whatStr(std::move(whatStr)) { }
+	BinaryHeap(const std::string& whatStr) noexcept : whatStr(whatStr) { }
+	~BinaryHeap() noexcept = default;
 };
-inline const char* BinarySearchTreeException::what() const noexcept
+inline const char* BinaryHeap::what() const noexcept
 {
 	return this->whatStr.c_str();
 };
