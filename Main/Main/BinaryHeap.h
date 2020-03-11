04243324081898b3e71a7887bad5dffb2a5e74dd
Ivan Morozov <38524976+ivanmorozfd@users.noreply.github.com>
2020-03-11 11:20:37 +0300

    refactor binaryheap

--- Main/Main/BinaryHeap.h
+++ Main/Main/BinaryHeap.h
@@ -1,48 +1,49 @@
-	#pragma once
+#pragma once
 #include <vector>
 #include "BinaryHeapExceptions.h"
 using std::vector;
-
+using std::initializer_list;
 template<typename _T>
 class BinaryHeap
 {
+private:
+	vector<_T> data;//contains values
 public:
-	vector<_T> data;
-	int getSize() 
+	//returns size of the heap
+	int getSize() const
 	{
 		return data.size(); 
 	}
-	void addItem(_T item)
+	//add item to heap
+	void addItem(const _T& item)
 	{
 		//push item to the value vector
 		data.push_back(item);
-		//get count and leaf parent
+		//get counter and leaf parent
 		int i = getSize() - 1;
 		int parent = (i - 1) / 2;
 		//the heap property may be violated
 		//while its violated we swap element with parent
 		while (i > 0 && data[parent] < data[i])	
 		{
-			int temp = data[i];
-			data[i] = data[parent];
-			data[parent] = temp;
+			std::swap(data[i],data[parent]);
 			i = parent;
 			parent = (i - 1) / 2;
 		}
 	}
 	//This method is recover general property of heap
-	void heapify(_T i)
+	void heapify(const _T& i)
 	{
 		int leftChild;
 		int rightChild;
 		int largestChild;
-
-		for (; ; )
+		//iterate while general property of heap is not true
+		for (;;)
 		{
 			leftChild = 2 * i + 1; // left child have 2i + 1 position in array
 			rightChild = 2 * i + 2; // right child have 2i + 2 position in array
 			largestChild = i; // set the largest child
-
+			//searchind childs that greater than parent
 			if (leftChild < getSize() && data[leftChild] > data[largestChild])
 			{
 				largestChild = leftChild;
@@ -61,6 +62,7 @@ public:
 			i = largestChild;
 		}
 	}
+	//get the max element of the heap and recover general purpose of the heap
 	_T getmax()
 	{
 		_T item;
@@ -69,8 +71,26 @@ public:
 		heapify(0);
 		return item;
 	}
-	BinaryHeap() 
+	void clear() 
 	{
-
+		this->data.clear();	
+	}
+public:
+	BinaryHeap(): 
+	data()
+	{
+		
+	}
+	BinaryHeap(const BinaryHeap<_T>& other)
+	{
+		
+	}
+	BinaryHeap(const initializer_list<_T>& data)
+	{
+		
+	}
+	~BinaryHeap()
+	{
+		this->clear();	
 	}
 };
